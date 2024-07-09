ed448c0274fcb02b29ada0e9541bf403055faf2a
Aympab <aympab@gmail.com>
2024-07-09 10:10:57 +0200

    upd

--- C/src/main.c
+++ C/src/main.c
@@ -168,7 +168,7 @@ int main(int argc, char *argv[]) {
     #pragma omp target teams distribute map(tofrom:adjacency_matrix, new_pagerank, pagerank)
     for (int i = 0; i < GRAPH_ORDER; i++) {
       // #pragma omp parallel for shared(adjacency_matrix, new_pagerank, pagerank) firstprivate(i) reduction(+:new_pagerank[i])
-      #pragma omp parallel for shared(adjacency_matrix)
+      #pragma omp parallel for shared(adjacency_matrix, new_pagerank)
       for (int j = 0; j < GRAPH_ORDER; j++) {
         if (adjacency_matrix[j][i] == 1.0) {
           int outdegree = 0;
