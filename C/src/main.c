85c35fa166fd2963aa558487f6b4460f4b0f79dd
Aympab <aympab@gmail.com>
2024-07-09 07:49:31 +0200

    upd

--- C/src/main.c
+++ C/src/main.c
@@ -77,7 +77,7 @@ void calculate_pagerank(double pagerank[]) {
 // #pragma omp target teams distribute map(tofrom : adjacency_matrix)
     for (int i = 0; i < GRAPH_ORDER; i++) {
 // #pragma omp parallel for shared(adjacency_matrix) private(outdegree)
-  #pragma omp parallel for shared(adjacency_matrix, new_pagerank, pagerank)
+  #pragma omp parallel for shared(adjacency_matrix, pagerank) reduction(+:new_pagerank[i])
       for (int j = 0; j < GRAPH_ORDER; j++) {
         if (adjacency_matrix[j][i] == 1.0) {
           int outdegree = 0;
